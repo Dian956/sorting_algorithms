4b2db98f6b699ab31493b2bd2e94107c3c4513e9
Dian956 <dianamwelu554@gmail.com>
2023-11-21 05:28:58 -0800

    sorting algorithms

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+* selection_sort - Sorts an array of integers using the Selection sort algorithm.
+* @array: The array to be sorted.
+* @size: The size of the array.
+*/
+void selection_sort(int *array, size_t size)
+{
+size_t outer_index, inner_index;
+int current_min, temp, min_index;
+
+for (outer_index = 0; outer_index < size; outer_index++)
+{
+current_min = array[outer_index];
+for (inner_index = outer_index + 1; inner_index < size; inner_index++)
+{
+if (current_min > array[inner_index])
+{
+current_min = array[inner_index];
+min_index = inner_index;
+}
+}
+if (current_min != array[outer_index])
+{
+temp = array[outer_index];
+array[outer_index] = current_min;
+array[min_index] = temp;
+print_array(array, size);
+}
+}
+}
